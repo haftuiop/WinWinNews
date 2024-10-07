b63a16e6b344c0af18bd1aca3140f3e3e22b5ebd
haftuiop <p3t3rbruc3@gmail.com>
2024-10-07 21:34:38 +0100

    rough layout of functions

--- TLDR.py
+++ TLDR.py
@@ -1,5 +1,7 @@
 import requests
 
+import feedparser
+
 
 tldr_podcast = "https://podcasts.watchnebula.com/thedailybriefingpodcast/febeea0cf8aa4953823b766ab42e035d"
 
@@ -10,12 +12,37 @@ tldr_podcast = "https://podcasts.watchnebula.com/thedailybriefingpodcast/febeea0
 
 
 
-last_date = [7,10,2024]
+
+
+last_date = [7,10,2024]#the last date we downloaded from...
 
 def GET_newPodcastFiles():
 
 
     files = []
+
+
+
+
+    return files
+
+
+def GET_goodNewsTimestamps():
+    ()
+    #look through the video for something on the same date? OR with the same title????
+    
+
+
+
+def mergeFiles(_files):
+    ()
+
+
+def uploadMergedVideo(_video):
+    ()
+    
+
+
     
 
 
