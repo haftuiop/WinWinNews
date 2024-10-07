f398d86663691d7575056fddfb58cf717639dadd
haftuiop <p3t3rbruc3@gmail.com>
2024-10-07 21:27:24 +0100

    tldr podcast

---
+++ TLDR.py
@@ -0,0 +1,22 @@
+import requests
+
+
+tldr_podcast = "https://podcasts.watchnebula.com/thedailybriefingpodcast/febeea0cf8aa4953823b766ab42e035d"
+
+
+
+
+
+
+
+
+last_date = [7,10,2024]
+
+def GET_newPodcastFiles():
+
+
+    files = []
+    
+
+
+    
